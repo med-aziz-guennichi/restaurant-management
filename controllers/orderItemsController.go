32bf091ddc71b8ddeeefa5a5820c8ddcb6612c46
Med Aziz Guennichi <medazizguennichi@gmail.com>
2024-01-20 14:28:33 -0800

    complete controllers struction

--- controllers/orderItemsController.go
+++ controllers/orderItemsController.go
@@ -1,1 +1,30 @@
 package controllers
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func GetOrderItemsByOrder() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetOrderItems() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func CreateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func UpdateOrderItem() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
