bff49972d610a7f84ae9a75e4087100781d619ae
Med Aziz Guennichi <medazizguennichi@gmail.com>
2024-01-20 15:13:16 -0800

    complete the models structions

--- models/orderModel.go
+++ models/orderModel.go
@@ -1,1 +1,16 @@
 package models
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Order struct {
+	ID         primitive.ObjectID `bson:"_id"`
+	Order_date time.Time          `json:"order_date"`
+	Created_at time.Time          `json:"created_at"`
+	Update_at  time.Time          `json:"update_at"`
+	Order_id   string             `json:"order_id"`
+	Table_id   *string            `json:"table_id" validate:"required"`
+}
